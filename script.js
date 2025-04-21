ab38a6df6c7c1d9919594cd47e7d1dacd0d18f18
TasleemAhmed2011 <tasleemahmed2011@gmail.com>
2025-04-21 16:19:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var bgcolor="rgb(" + r + ", " + g + ", " + b + ")";
+    document.body.style.backgroundColor = bgcolor;
+}
